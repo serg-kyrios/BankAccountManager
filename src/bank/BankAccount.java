a78bb53f50f0ae65b37318dfa2daea42fa862c5d
Sergey KYRYLIUK <sergkyrios@gmail.com>
2026-01-15 18:35:57 +0200

    add bank package and base classes

---
+++ src/bank/BankAccount.java
@@ -0,0 +1,15 @@
+package bank;
+import java.util.List;
+import java.util.ArrayList;
+
+public class BankAccount {
+          String ownerName;
+          int  balance;
+    List<Transaction> transactions;
+
+    double deposit(double amount){};
+
+    double withdraw(double amount){};
+
+   List printTransactions(){};
+}
